d2da76cfa8a0f1110715d5318cd688fae90622c4
Bensson <47039664+SiTronXD@users.noreply.github.com>
2022-07-18 20:08:44 +0200

    more swap chain

--- Kronos/Main.cpp
+++ Kronos/Main.cpp
@@ -313,6 +313,43 @@ private:
 			swapChainAdequate;
 	}
 
+	VkSurfaceFormatKHR chooseSwapSurfaceFormat(
+		const std::vector<VkSurfaceFormatKHR>& availableFormats)
+	{
+		// Find specific format/color space combination
+		for (const auto& availableFormat : availableFormats)
+		{
+			if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB &&
+				availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+			{
+				return availableFormat;
+			}
+		}
+
+		// Use the first format/color space combination
+		Log::warning("First surface format was chosen.");
+		return availableFormats[0];
+	}
+
+	VkPresentModeKHR chooseSwapPresentMode(
+		const std::vector<VkPresentModeKHR>& availablePresentModes)
+	{
+		// Find specific present mode
+		for (const auto& availablePresentMode : availablePresentModes)
+		{
+			if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR)
+				return availablePresentMode;
+		}
+		
+		// Guaranteed to be available
+		return VK_PRESENT_MODE_FIFO_KHR;
+	}
+
+	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities)
+	{
+
+	}
+
 	SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device)
 	{
 		SwapChainSupportDetails details;
