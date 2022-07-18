b837d48f023415a8a8fe43394eb86c441016333e
Bensson <47039664+SiTronXD@users.noreply.github.com>
2022-07-18 21:16:26 +0200

    swap chain extents

--- Kronos/Main.cpp
+++ Kronos/Main.cpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include <optional>
 #include <set>
+#include <algorithm>
+#include <limits>
 
 #include "Engine/Dev/Log.h"
 
@@ -125,6 +127,7 @@ private:
 		createSurface();
 		pickPhysicalDevice();
 		createLogicalDevice();
+		createSwapChain();
 	}
 
 	void createInstance()
@@ -347,7 +350,34 @@ private:
 
 	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities)
 	{
+		if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max())
+		{
+			return capabilities.currentExtent;
+		}
+		else
+		{
+			int width, height;
+			glfwGetFramebufferSize(this->window, &width, &height);
 
+			VkExtent2D actualExtent =
+			{
+				static_cast<uint32_t>(width),
+				static_cast<uint32_t>(height)
+			};
+
+			actualExtent.width = std::clamp(
+				actualExtent.width, 
+				capabilities.minImageExtent.width,
+				capabilities.maxImageExtent.width
+			);
+			actualExtent.height = std::clamp(
+				actualExtent.height,
+				capabilities.minImageExtent.height,
+				capabilities.maxImageExtent.height
+			);
+
+			return actualExtent;
+		}
 	}
 
 	SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device)
@@ -535,6 +565,57 @@ private:
 
 		// Get present queue handle
 		vkGetDeviceQueue(this->device, indices.presentFamily.value(), 0, &this->presentQueue);
+	}
+
+	void createSwapChain()
+	{
+		// Swap chain support
+		SwapChainSupportDetails swapChainSupport = 
+			querySwapChainSupport(this->physicalDevice);
+
+		// Format, present mode and extent
+		VkSurfaceFormatKHR surfaceFormat = chooseSwapSurfaceFormat(swapChainSupport.formats);
+		VkPresentModeKHR presentMode = chooseSwapPresentMode(swapChainSupport.presentModes);
+		VkExtent2D extent = chooseSwapExtent(swapChainSupport.capabilities);
+
+		// Image count
+		uint32_t imageCount = swapChainSupport.capabilities.minImageCount + 1;
+		if (swapChainSupport.capabilities.maxImageCount > 0 && imageCount > swapChainSupport.capabilities.maxImageCount)
+		{
+			imageCount = swapChainSupport.capabilities.maxImageCount;
+		}
+
+		// Swap chain create info
+		VkSwapchainCreateInfoKHR createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+		createInfo.surface = this->surface;
+
+		createInfo.minImageCount = imageCount;
+		createInfo.imageFormat = surfaceFormat.format;
+		createInfo.imageColorSpace = surfaceFormat.colorSpace;
+		createInfo.imageExtent = extent;
+		createInfo.imageArrayLayers = 1;
+		createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+		// How swap chain images are used across multiple queue families
+		QueueFamilyIndices indices = findQueueFamilies(this->physicalDevice);
+		uint32_t queueFamilyIndices[] = 
+		{ 
+			indices.graphicsFamily.value(),
+			indices.presentFamily.value()
+		};
+		if (indices.graphicsFamily != indices.presentFamily)
+		{
+			createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+			createInfo.queueFamilyIndexCount = 2;
+			createInfo.pQueueFamilyIndices = queueFamilyIndices;
+		}
+		else
+		{
+			createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+			createInfo.queueFamilyIndexCount = 0; // Optional
+			createInfo.pQueueFamilyIndices = nullptr; // Optional
+		}
 	}
 
 	void mainLoop()
